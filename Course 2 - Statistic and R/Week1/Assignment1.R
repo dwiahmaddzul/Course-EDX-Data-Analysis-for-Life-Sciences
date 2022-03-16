f9b9977ae798abd8a9c1287b157da79594a766dd
Dwi Ahmad Dzulhijjah <68468571+dwiahmaddzul@users.noreply.github.com>
2022-03-16 09:22:51 +0700

    menambah test

--- Course 2 - Statistic and R/Week1/Assignment1.R
+++ Course 2 - Statistic and R/Week1/Assignment1.R
@@ -1,3 +1,35 @@
+#QUest1
 data <- c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
 mean(data)
 
+#Quest2
+sum <- 0
+
+for (i in 1:25){
+  
+  i <- i^2
+  
+  sum <- i+ sum 
+  
+}
+
+sum
+
+#Quest4
+cars
+class(cars)
+
+#QUest5
+nrow(cars)
+
+#Quest6
+colnames(cars)
+
+#Quest7
+avg <- 0
+avg <- mean(cars[,2])
+avg
+
+#quest8
+hasil <- which(cars$dist==85)
+hasil
